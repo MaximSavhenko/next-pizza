618514d32bf691d7fb92737442d6e42349f4798d
Maxim <uspmsm@gmail.com>
2024-11-13 13:54:24 +0200

    add filters and product card components

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { TopBar, Container, Title } from '@/components/shared'
+import { TopBar, Container, Title, Filters, ProductCard } from '@/components/shared'
 
 export default function Home() {
   return (
@@ -8,7 +8,19 @@ export default function Home() {
       </Container>
 
       <TopBar />
-      <div style={{height: '2000px'}}></div>
+
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          <div className="w-[250px]">
+            <Filters/>
+          </div>
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              <ProductCard id={0} name={'Сырный цыпленок'} price={200} imageUrl={'https://adminbm.kharkiv.ua/uploads/DSC_09546_3fcd539887.webp'} />
+            </div>
+          </div>
+        </div>
+      </Container>
     </>
   )
 }
