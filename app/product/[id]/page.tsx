5f4aa3b465fc264160fd071f4fa2a833dfe24f90
Maxim <uspmsm@gmail.com>
2024-11-27 01:43:00 +0200

    working on the product page, displaying product pictures

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,7 +1,63 @@
-export default function ProductPage({
+'use client'
+
+import {
+  Container,
+  GroupVariants,
+  ProductImage,
+  Title,
+} from '@/components/shared'
+import { prisma } from '@/prisma/prisma-client'
+import { notFound } from 'next/navigation'
+
+export default async function ProductPage({
   params: { id },
 }: {
   params: { id: string }
 }) {
-  return <p>Product {id}</p>
+  
+
+  const product = await prisma.product.findFirst({ where: { id: Number(id) } })
+
+  if (!product) {
+    return notFound()
+  }
+
+  return (
+    <Container className="flex flex-col my-10">
+      <div className="flex flex-1">
+        <ProductImage imageUrl={product.imageUrl} className="" size={40} />
+
+        <div className="w-[490px] bg-[#FFF7EE] p-7">
+          <Title
+            text={product.name}
+            size="md"
+            className="font-extrabold mb-1"
+          />
+          <p className="text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
+            quam repudiandae omnis blanditiis quis deserunt optio consequatur
+            ratione quibusdam fugit nostrum adipisci, molestiae, ab placeat,
+            dicta sit nemo rem dolorem!
+          </p>
+          <GroupVariants
+          selectedValue='2'
+            items={[
+              {
+                name: 'Маленькая',
+                value: '1',
+              },
+              {
+                name: 'Средняя',
+                value: '2',
+              },
+              {
+                name: 'Большая',
+                value: '3',
+              },
+            ]}
+          />
+        </div>
+      </div>
+    </Container>
+  )
 }
