451f952748a15a844e36544d483805af6394f1ca
Maxim <uspmsm@gmail.com>
2024-11-28 02:28:23 +0200

    working on a modal window, layout, passing props

--- components/shared/modals/choose-product-modal.tsx
+++ components/shared/modals/choose-product-modal.tsx
@@ -1,14 +1,12 @@
 'use client'
 
-import {
-  Dialog,
-  DialogContent,
-} from '@/components/ui/dialog'
+import { Dialog, DialogContent } from '@/components/ui/dialog'
 import { cn } from '@/lib/utils'
 import { Product } from '@prisma/client'
 import React from 'react'
 import { Title } from '../title'
 import { useRouter } from 'next/navigation'
+import { ChooseProductForm } from '../choose-product-form'
 
 interface Props {
   className?: string
@@ -16,18 +14,21 @@ interface Props {
 }
 
 export const ChooseProductModal: React.FC<Props> = ({ className, product }) => {
-
   const router = useRouter()
-  
+
   return (
-    <Dialog open={Boolean(product)} onOpenChange={()=> router.back()}>
+    <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
       <DialogContent
         className={cn(
           'p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden',
           className
         )}
       >
-          <Title text={product.name} />
+        <ChooseProductForm
+          imageUrl={product.imageUrl}
+          name={product.name}
+          ingredients={[]}
+        />
       </DialogContent>
     </Dialog>
   )
