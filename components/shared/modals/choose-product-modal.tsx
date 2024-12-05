e21ff69f516c2a65566751c2097517c408b83278
Maxim Savchenko <maximsavchenko@MacBook-Air-Maxim.local>
2024-12-05 10:20:34 +0200

    fix modal product

--- components/shared/modals/choose-product-modal.tsx
+++ components/shared/modals/choose-product-modal.tsx
@@ -4,17 +4,19 @@ import { Dialog, DialogContent } from '@/components/ui/dialog'
 import { cn } from '@/lib/utils'
 import { Product } from '@prisma/client'
 import React from 'react'
-import { Title } from '../title'
 import { useRouter } from 'next/navigation'
 import { ChooseProductForm } from '../choose-product-form'
+import { ProductWithRelations } from '@/@types/prisma'
 
 interface Props {
   className?: string
-  product: Product
+  product: ProductWithRelations
 }
 
 export const ChooseProductModal: React.FC<Props> = ({ className, product }) => {
   const router = useRouter()
+
+  const isPizzaForrm = Boolean(product.items[0].pizzaType)
 
   return (
     <Dialog open={Boolean(product)} onOpenChange={() => router.back()}>
@@ -24,11 +26,15 @@ export const ChooseProductModal: React.FC<Props> = ({ className, product }) => {
           className
         )}
       >
-        <ChooseProductForm
-          imageUrl={product.imageUrl}
-          name={product.name}
-          ingredients={[]}
-        />
+        {isPizzaForrm ? (
+          'Pizza'
+        ) : (
+          <ChooseProductForm
+            imageUrl={product.imageUrl}
+            name={product.name}
+            ingredients={[]}
+          />
+        )}
       </DialogContent>
     </Dialog>
   )
