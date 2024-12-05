c1d3f0fc07cceffffc26d0eac647ff2f424dddcb
Maxim Savchenko <maximsavchenko@MacBook-Air-Maxim.local>
2024-12-05 10:20:34 +0200

    fix modal product

--- components/shared/choose-product-form.tsx
+++ components/shared/choose-product-form.tsx
@@ -26,14 +26,20 @@ export const ChooseProductForm: React.FC<Props> = ({
   const totalPrice = 200
   return (
     <div className={cn(className, 'flex flex-1')}>
-      <ProductImage imageUrl={imageUrl} size={30} />
+      <div className="flex flex-1 items-center justify-center relative w-full">
+        <img
+          src={imageUrl}
+          alt={name}
+          className="relative left-2 top-2 transition-all z-10 duration-300 w-[350px] h-[350px]"
+        />
+      </div>
 
       <div className="w-[490px] bg-[#FFF7EE] p-7">
         <Title text={name} size="md" className="font-extrabold mb-1" />
 
         <p className="text-gray-400">{textDetaills}</p>
 
-        <Button className="h-[55px] px-10 text-base rounded-[18px] w-full">
+        <Button className="h-[55px] px-10 text-base rounded-[18px] w-full mt-10">
           Добавить в корзину за {totalPrice} ₴
         </Button>
       </div>
