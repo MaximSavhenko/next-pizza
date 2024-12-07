559c95a173508b904896681f03e6d5975e463194
Maxim <uspmsm@gmail.com>
2024-12-07 13:12:05 +0200

    Added 'Shared' folder into which I moved all components, hooks, constants, lib folder, etc. Added ingredients to the modal window, select the size and type of pizza

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import {
   Title,
   Filters,
   ProductsGroupList,
-} from '@/components/shared'
+} from '@/shared/components/shared'
 import { prisma } from '@/prisma/prisma-client'
 
 export default async function Home() {
