a42265fc52c1c2939798e1dec2df24010d659114
Maxim Savchenko <maximsavchenko@MacBook-Air-Maxim.local>
2025-01-10 22:20:12 +0200

    Fixed a bug with adding pizzas with different ingredients to the cart, added layout with an empty cart and logic for it

--- shared/components/shared/cart-drawer-item.tsx
+++ shared/components/shared/cart-drawer-item.tsx
@@ -5,9 +5,9 @@ import { CartItemProps } from './cart-item-details/cart-item-details.types'
 import { Trash2Icon } from 'lucide-react'
 
 interface Props extends CartItemProps {
-  className?: string,
-  onClickCountButton?: (type: 'plus' | 'minus') => void;
-  onClickRemoveItem?: () => void;
+  className?: string
+  onClickCountButton?: (type: 'plus' | 'minus') => void
+  onClickRemoveItem?: () => void
 }
 
 export const CartDrawerItem: React.FC<Props> = ({
@@ -16,12 +16,21 @@ export const CartDrawerItem: React.FC<Props> = ({
   price,
   quantity,
   details,
+  disabled,
   onClickCountButton,
   onClickRemoveItem,
   className,
 }) => {
+  console.log(disabled);
+  
   return (
-    <div className={cn('flex bg-white p-5 gap-6', className)}>
+    <div
+      className={cn(
+        'flex bg-white p-5 gap-6',
+        { 'opacity-50 pointer-events-none': disabled },
+        className
+      )}
+    >
       <CartItem.Image src={imageUrl} />
       <div className="flex-1">
         <CartItem.Info name={name} details={details} />
@@ -29,10 +38,7 @@ export const CartDrawerItem: React.FC<Props> = ({
         <hr className="my-3" />
 
         <div className="flex items-center justify-between">
-          <CartItem.CountButton
-            onClick={onClickCountButton}
-            value={quantity}
-          />
+          <CartItem.CountButton onClick={onClickCountButton} value={quantity} />
 
           <div className="flex items-center gap-3">
             <CartItem.Price value={price} />
