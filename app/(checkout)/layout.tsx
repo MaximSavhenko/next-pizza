6f1101ca3f53c6a93e64a7433ec73c4b20cdedbb
Maxim <uspmsm@gmail.com>
2025-01-24 18:08:08 +0200

    add suspens for checkout header

--- app/(checkout)/layout.tsx
+++ app/(checkout)/layout.tsx
@@ -1,4 +1,5 @@
 import { Container, Header } from '@/shared/components/shared'
+import { Suspense } from 'react'
 
 export const metadata = {
   title: 'Next Pizza | Корзина',
@@ -13,7 +14,13 @@ export default function CheckoutLayout({
   return (
     <main className="min-h-screen bg-[#F4F1EE]">
       <Container>
-        <Header hasSearch={false} hasCart={false} className="border-b-gray-200" />
+        <Suspense>
+          <Header
+            hasSearch={false}
+            hasCart={false}
+            className="border-b-gray-200"
+          />
+        </Suspense>
         {children}
       </Container>
     </main>
